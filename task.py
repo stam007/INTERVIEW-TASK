1837c4770a84407c9bb26c8e2894e4a6b459f0a4
stam007 <nourdin--nacer@hotmail.com>
2021-11-26 21:37:16 +0100

    main

--- task.py
+++ task.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import json
-path_input_text="input_file.txt"
-path_standard_definition_json="standard_definition.json"
-path_error_codes="error_codes.json"
+import logging
+import os
+import unittest
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+os.environ['path_input_text'] ="input_file.txt"
+os.environ['path_standard_definition_json']="standard_definition.json"
+os.environ['path_error_codes']="error_codes.json"
+os.environ['path_parsed_summary']="parsed/summary.txt"
+os.environ['path_parsed_report']='parsed/report.csv'
+
+
 
 
 #REPORT DICT
@@ -10,15 +22,15 @@ report={"Section":[],"Sub-Section":[],"Given DataType":[],"Expected DataType":[]
 
 
 #LOAD THE STANDARD DEFINITION
-f = open(path_standard_definition_json,)
+f = open(os.environ.get('path_standard_definition_json'),)
 standard = json.load(f)
-print("STANDARD",standard) 
+logger.debug("STANDARD DATA : %s", standard) 
 f.close()
 
 #LOAD THE ERROR CODES
-m = open(path_error_codes,)
+m = open(os.environ.get('path_error_codes'),)
 errors = json.load(m)
-print("ERRORS",errors) 
+logger.debug("ERRORS DATA: %s", errors) 
 m.close()
 
 
@@ -65,6 +77,7 @@ def make_report(index,ln,sec_name,sub_sec):
     val_sub_sec=ln[index]
   except:
     val_sub_sec= None
+    logger.exception("No Data Found in Index %d",index) 
     
   given_dataype =None if val_sub_sec==None else get_data_type(val_sub_sec)
   given_lenth = None if val_sub_sec==None else len(val_sub_sec)
@@ -77,53 +90,58 @@ def make_report(index,ln,sec_name,sub_sec):
   report["Expected MaxLength"].append(sub_sec['max_length'])
   report["Error Code"].append(error_code)
      
-  return
+  return True
 
 
 
 #EXPORT REPORT TO parsed/report.csv
 def export_report(df):
 
-  df.to_csv('parsed/report.csv',index=False,float_format='%.0f')
+  df.to_csv(os.environ.get('path_parsed_report'),index=False,float_format='%.0f')
+  return True
 
 
 #EXPORT SUMMARY TO parsed/summary.text
 def export_summary(df):
-  file1 = open("parsed/summary.txt","w")
+  file1 = open(os.environ.get('path_parsed_summary'),"w")
 
   for index, row in df.iterrows():
     err = [d for d in errors if d['code']==row['Error Code']][0]["message_template"].replace("LXY",row['Sub-Section']).replace("LX",row['Section']).replace("{data_type}",row['Expected DataType']).replace("{max_length}",str(row['Expected MaxLength']))
     file1.write(err+" \n")
-    print(err)
+    logger.debug("SUMMARY : %s", err) 
 
   file1.close()
-  return
+  return True
 
 
 
 #RUN TASK & LOAD INPUT TEXT
-def Run_Task():
-  f = open(path_input_text, "r")
-  rs = f.read()
-  rs=rs.split("\n")
+class MyTask(unittest.TestCase):
+  def test_run_task(self):
+    f = open(os.environ.get('path_input_text') , "r")
+    rs = f.read()
+    rs=rs.split("\n")
 
-  for x in rs:
-    ln= x.split("&")
-    if len(ln)>0:
-      key=ln[0]
-      stand= [d for d in standard if d['key']==key]
-      if len(stand)>0:
-        for k in stand[0]["sub_sections"]:
-          index = int(k['key'].replace(key,''))
-          make_report(index,ln,key,k)
-  
-  print("REPORT",report)
-  df = pd.DataFrame(report)
-  export_report(df)
-  export_summary(df)
+    for x in rs:
+      ln= x.split("&")
+      if len(ln)>0:
+        key=ln[0]
+        stand= [d for d in standard if d['key']==key]
+        if len(stand)>0:
+          for k in stand[0]["sub_sections"]:
+            index = int(k['key'].replace(key,''))
+            self.assertTrue(make_report(index,ln,key,k),"Test Make Report Fail")
+    
+    logger.debug("REPORT : %s", report)
+    f.close() 
+    df = pd.DataFrame(report)
+    self.assertTrue(export_report(df),"Test Export Report Fail")
+    self.assertTrue(export_summary(df),"Test Export Summary Fail")
+    
 
 
 
-Run_Task()
+if __name__ == '__main__':
+  unittest.main()
 
-  
+
